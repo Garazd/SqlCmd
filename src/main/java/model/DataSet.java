32bc4c4bc8dc2605e3de0f8d5e7efefcf2189c1e
Garazd <GarazdVZ@gmail.com>
2016-06-01 00:23:59 +0300

    Refactoring DataSet

---
+++ src/main/java/model/DataSet.java
@@ -0,0 +1,75 @@
+package model;
+
+import java.util.Arrays;
+
+public class DataSet {
+    static class Data {
+        private String name;
+        private Object value;
+
+        public Data(String name, Object value) {
+            this.name = name;
+            this.value = value;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public Object getValue() {
+            return value;
+        }
+    }
+
+    public Data[] data = new Data[100];
+    public int freeIndex;
+
+    public void put (String name, Object value) {
+        for (int index = 0; index < freeIndex; index++) {
+            if (data[index].getName().equals(name)) {
+                data[index].value = value;
+            }
+        }
+        data[freeIndex++] = new Data(name, value);
+    }
+
+    public String[] getNames() {
+        String[] result = new String[freeIndex];
+        for (int i = 0; i < freeIndex; i++) {
+            result[i] = data[i].getName();
+        }
+        return result;
+    }
+
+    public Object[] getValues() {
+        Object[] result = new Object[freeIndex];
+        for (int i = 0; i < freeIndex; i++) {
+            result[i] = data[i].getValue();
+        }
+        return result;
+    }
+
+    public Object get(String name) {
+        for (int i = 0; i < freeIndex; i++) {
+            if (data[i].getName().equals(name)) {
+                return data[i].getValue();
+            }
+        }
+        return null;
+    }
+
+    public void updateFrom(DataSet newValue) {
+        for (int index = 0; index < newValue.freeIndex; index++) {
+            Data data = newValue.data[index];
+            this.put(data.name, data.value);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "DatSet{\n" +
+            "names:" + Arrays.toString(getNames()) + "\n" +
+            "values:" + Arrays.toString(getValues()) + "\n" +
+            "}";
+    }
+}
