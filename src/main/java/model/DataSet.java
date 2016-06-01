3b62ca4b959c2f7eb02ea33f44d6c64a024cd21f
Garazd <GarazdVZ@gmail.com>
2016-06-02 01:20:43 +0300

    Refactoring Main and DataSet

--- src/main/java/model/DataSet.java
+++ src/main/java/model/DataSet.java
@@ -22,12 +22,13 @@ public class DataSet {
     }
 
     public Data[] data = new Data[100];
-    public int freeIndex;
+    public int freeIndex = 0;
 
     public void put (String name, Object value) {
         for (int index = 0; index < freeIndex; index++) {
             if (data[index].getName().equals(name)) {
                 data[index].value = value;
+                return;
             }
         }
         data[freeIndex++] = new Data(name, value);
