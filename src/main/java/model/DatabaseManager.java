904682e5e812269788d564d346f1217b1053c239
Garazd <GarazdVZ@gmail.com>
2016-06-02 01:17:58 +0300

    Extract interface DatabaseManager

---
+++ src/main/java/model/DatabaseManager.java
@@ -0,0 +1,18 @@
+package model;
+
+public interface DatabaseManager {
+
+    void create(String tableName, DataSet input);
+
+    void update(String tableName, int id, DataSet newValue);
+
+    void clear(String tableName);
+
+    String[] getTableNames();
+
+    DataSet[] getTableData(String tableName);
+
+    void connect(String database, String userName, String password);
+
+    String[] getTableColumns(String tableName);
+}
