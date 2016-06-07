4a9dcd1695197160008d3b779c7e0d4640f5a21a
Garazd <GarazdVZ@gmail.com>
2016-06-08 00:30:18 +0300

    Added command find

---
+++ src/main/java/controller/command/Find.java
@@ -0,0 +1,59 @@
+package controller.command;
+
+import model.DataSet;
+import model.DatabaseManager;
+import view.View;
+
+public class Find implements Command {
+
+    private DatabaseManager manager;
+    private View view;
+
+    public Find(DatabaseManager manager, View view) {
+        this.manager = manager;
+        this.view = view;
+    }
+
+    @Override
+    public boolean canProcess(String command) {
+        return command.startsWith("find|");
+    }
+
+    @Override
+    public void process(String command) {
+        String[] data = command.split("\\|");
+        String tableName = data[1];
+
+        String[] tableColumns = manager.getTableColumns(tableName);
+        printHeader(tableColumns);
+
+        DataSet[] tableData = manager.getTableData(tableName);
+        printTable(tableData);
+    }
+
+    private void printTable(DataSet[] tableData) {
+        for (DataSet row : tableData) {
+            printRow(row);
+        }
+        view.write("--------------------");
+    }
+
+    private void printRow(DataSet row) {
+        Object[] values = row.getValues();
+        String result = "|";
+        for (Object value : values) {
+            result += value + "|";
+        }
+        view.write(result);
+    }
+
+    private void printHeader(String[] tableColmns) {
+        String result = "|";
+        for (String name : tableColmns) {
+            result += name + "|";
+        }
+        view.write("---------------------");
+        view.write(result);
+        view.write("---------------------");
+    }
+}
