882fc8fb20670653589663e01f8ecc429f87f4ff
Garazd <GarazdVZ@gmail.com>
2016-06-14 00:34:09 +0300

    Add ExitTest

---
+++ src/test/java/controller/command/ExitTest.java
@@ -0,0 +1,54 @@
+package controller.command;
+
+import org.junit.Test;
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+public class ExitTest {
+
+    private FakeView view = new FakeView();
+
+    @Test
+    public void testCanProcessExitString() {
+        //given
+        Command command = new Exit(view);
+
+        //when
+        boolean canProcess = command.canProcess("exit");
+
+        //then
+        assertTrue(canProcess);
+    }
+
+    @Test
+    public void testCanProcessQweString() {
+        //given
+        Command command = new Exit(view);
+
+        //when
+        boolean canProcess = command.canProcess("qwe");
+
+        //then
+        assertFalse(canProcess);
+    }
+
+    @Test
+    public void testProcessExitCommandThrowsExitException() {
+        //given
+        Command command = new Exit(view);
+
+        //when
+        try {
+            command.process("qwe");
+            fail("Expected ExitException");
+        } catch (ExitException e) {
+            //do nothing;
+        }
+
+        //then
+        assertEquals("See you later!\n", view.getContent());
+        //throws
+    }
+}
