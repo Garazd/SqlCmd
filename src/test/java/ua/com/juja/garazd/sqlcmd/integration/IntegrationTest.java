42b8a1b4302ddeba8c433bc1534192bb499af7bb
Garazd <GarazdVZ@gmail.com>
2016-08-28 00:05:34 +0300

    Refactoring rename class and methods

--- src/test/java/ua/com/juja/garazd/sqlcmd/integration/IntegrationTest.java
+++ src/test/java/ua/com/juja/garazd/sqlcmd/integration/IntegrationTest.java
@@ -25,7 +25,7 @@ public class IntegrationTest {
     @Before
     public void setup() {
         manager = new JDBCDatabaseManager();
-        manager.connect(DATABASE_NAME, USER_NAME, PASSWORD);
+        manager.connectDatabase(DATABASE_NAME, USER_NAME, PASSWORD);
         in = new ConfigurableInputStream();
         out = new ByteArrayOutputStream();
 
@@ -46,7 +46,7 @@ public class IntegrationTest {
     @Test
     public void testClear() {
         // given
-        in.add("clear|user");
+        in.add("clearTable|user");
         in.add("exit");
 
         // when
@@ -55,7 +55,7 @@ public class IntegrationTest {
         // then
         assertEquals("Connecting to a database is successful\r\n" +
             "Enter command (or help for help):\r\n" +
-            // clear|user
+            // clearTable|user
             "Table user has been successfully cleared.\r\n" +
             // exit
             "See you later! Bye\r\n", getData());
@@ -64,7 +64,7 @@ public class IntegrationTest {
     @Test
     public void testClearWithError() {
         // given
-        in.add("clear|sadfasd|fsf|fdsf");
+        in.add("clearTable|sadfasd|fsf|fdsf");
         in.add("exit");
 
         // when
@@ -73,8 +73,8 @@ public class IntegrationTest {
         // then
         assertEquals("Connecting to a database is successful\r\n" +
             "Enter command (or help for help):\r\n" +
-            // clear|sadfasd|fsf|fdsf
-            "Failure! because of: command format is 'clear|tableName', and you have brought: clear|sadfasd|fsf|fdsf\r\n" +
+            // clearTable|sadfasd|fsf|fdsf
+            "Failure! because of: command format is 'clearTable|tableName', and you have brought: clearTable|sadfasd|fsf|fdsf\r\n" +
             "Please try again.\r\n" +
             // exit
             "See you later! Bye\r\n", getData());
@@ -83,9 +83,9 @@ public class IntegrationTest {
     @Test
     public void testClearAndCreateTableData() {
         // given
-        in.add("clear|user");
-        in.add("create|user|id|13|name|Stiven|password|*****");
-        in.add("create|user|id|14|name|Eva|password|+++++");
+        in.add("clearTable|user");
+        in.add("createTable|user|id|13|name|Stiven|password|*****");
+        in.add("createTable|user|id|14|name|Eva|password|+++++");
         in.add("find|user");
         in.add("exit");
 
@@ -95,11 +95,11 @@ public class IntegrationTest {
         // then
         assertEquals("Connecting to a database is successful\r\n" +
             "Enter command (or help for help):\r\n" +
-            // clear|user
+            // clearTable|user
             "Table user has been successfully cleared.\r\n" +
-            // create|user|id|13|name|Stiven|password|*****
+            // createTable|user|id|13|name|Stiven|password|*****
             "Recording {names:[id, name, password], values:[13, Stiven, *****]} was successfully created in the table 'user'.\r\n" +
-            // create|user|id|14|name|Eva|password|+++++
+            // createTable|user|id|14|name|Eva|password|+++++
             "Recording {names:[id, name, password], values:[14, Eva, +++++]} was successfully created in the table 'user'.\r\n" +
             // find|user
             "--------------------\r\n" +
@@ -115,7 +115,7 @@ public class IntegrationTest {
     @Test
     public void testCreateWithErrors() {
         // given
-        in.add("create|user|error");
+        in.add("createTable|user|error");
         in.add("exit");
 
         // when
@@ -124,8 +124,8 @@ public class IntegrationTest {
         // then
         assertEquals("Connecting to a database is successful\r\n" +
             "Enter command (or help for help):\r\n" +
-            // create|user|error
-            "Failure! because of: Must be an even number of parameters in a format 'create|tableName|column1|value1|column2|value2|...|columnN|valueN', but you sent: 'create|user|error'\r\n" +
+            // createTable|user|error
+            "Failure! because of: Must be an even number of parameters in a format 'createTable|tableName|column1|value1|column2|value2|...|columnN|valueN', but you sent: 'createTable|user|error'\r\n" +
             "Please try again.\r\n" +
             // exit
             "See you later! Bye\r\n", getData());
@@ -183,10 +183,10 @@ public class IntegrationTest {
             "Existing command:\r\n" +
             "\tlist\r\n" +
             "\t\tfor a list of all database tables, is connected to\r\n" +
-            "\tclear|tableName\r\n" +
+            "\tclearTable|tableName\r\n" +
             "\t\tto clean up the entire table\r\n" +
-            "\tcreate|tableName|column1|value1|column2|value2|...|columnN|valueN\r\n" +
-            "\t\tto create a record in the table\r\n" +
+            "\tcreateTable|tableName|column1|value1|column2|value2|...|columnN|valueN\r\n" +
+            "\t\tto createTable a record in the table\r\n" +
             "\tfind|tableName\r\n" +
             "\t\tto get the contents of the table 'tableName'\r\n" +
             "\thelp\r\n" +
@@ -238,7 +238,7 @@ public class IntegrationTest {
     public void testNonexistentCommand() {
         // given
         in.add("list");
-        in.add("connect");
+        in.add("connectDatabase");
         in.add("list");
         in.add("exit");
 
@@ -250,8 +250,8 @@ public class IntegrationTest {
             "Enter command (or help for help):\r\n" +
             // list
             "[user, test]\r\n" +
-            // connect
-            "Nonexistent command: connect\r\n" +
+            // connectDatabase
+            "Nonexistent command: connectDatabase\r\n" +
             "Enter command (or help for help):\r\n" +
             // list
             "[user, test]\r\n" +
