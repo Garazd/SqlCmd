bb94f193aa9f17fb9c70ddd09b8a0ce23f7dc7bd
Garazd <GarazdVZ@gmail.com>
2016-08-29 22:21:49 +0300

    Refactoring rename class

--- src/main/java/ua/com/juja/garazd/sqlcmd/view/Console.java
+++ src/main/java/ua/com/juja/garazd/sqlcmd/view/Console.java
@@ -2,7 +2,7 @@ package ua.com.juja.garazd.sqlcmd.view;
 
 import java.util.Scanner;
 
-public class Concole implements View {
+public class Console implements View {
     @Override
     public void write(final String massage) {
         System.out.println(massage);
