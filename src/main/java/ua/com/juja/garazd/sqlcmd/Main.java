cc0b108fbde90a00316f6952494bb22bee27e25e
Garazd <GarazdVZ@gmail.com>
2016-08-29 22:22:23 +0300

    Refactoring class

--- src/main/java/ua/com/juja/garazd/sqlcmd/Main.java
+++ src/main/java/ua/com/juja/garazd/sqlcmd/Main.java
@@ -3,12 +3,12 @@ package ua.com.juja.garazd.sqlcmd;
 import ua.com.juja.garazd.sqlcmd.controller.MainController;
 import ua.com.juja.garazd.sqlcmd.model.DatabaseManager;
 import ua.com.juja.garazd.sqlcmd.model.JDBCDatabaseManager;
-import ua.com.juja.garazd.sqlcmd.view.Concole;
+import ua.com.juja.garazd.sqlcmd.view.Console;
 import ua.com.juja.garazd.sqlcmd.view.View;
 
 public class Main {
     public static void main(String[] args) {
-        View view = new Concole();
+        View view = new Console();
         DatabaseManager manager = new JDBCDatabaseManager();
 
         MainController controller = new MainController(manager, view);
