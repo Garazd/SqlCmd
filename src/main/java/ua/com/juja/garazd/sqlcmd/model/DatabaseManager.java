5e02c575ebf7478128d92ca09b963b8009cf9ca9
Garazd <GarazdVZ@gmail.com>
2016-08-31 22:23:08 +0300

    Added new method

--- src/main/java/ua/com/juja/garazd/sqlcmd/model/DatabaseManager.java
+++ src/main/java/ua/com/juja/garazd/sqlcmd/model/DatabaseManager.java
@@ -5,21 +5,27 @@ import java.util.Set;
 
 public interface DatabaseManager {
 
+    void connectDatabase(String database, String userName, String password);
+
+    void createDatabase (String databaseName);
+
+    void dropDatabase (String databaseName);
+
+    boolean isConnected();
+
+
     void createTable(String tableName, DataSet input);
+
+    void dropTable(String tableName);
 
     void updateTable(String tableName, int id, DataSetImpl newValue);
 
     void clearTable(String tableName);
 
+
     Set<String> getTableNames();
 
     List<DataSet> getTableData(String tableName);
 
-    int getSize(String tableName);
-
-    void connectDatabase(String database, String userName, String password);
-
     Set<String> getTableColumns(String tableName);
-
-    boolean isConnected();
 }
