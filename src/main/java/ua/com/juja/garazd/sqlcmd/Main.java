e6bec53788d2a45096953694f4f738950e9c4fde
Garazd <GarazdVZ@gmail.com>
2016-09-04 23:16:54 +0300

    Refactoring rename methods

--- src/main/java/ua/com/juja/garazd/sqlcmd/Main.java
+++ src/main/java/ua/com/juja/garazd/sqlcmd/Main.java
@@ -2,14 +2,14 @@ package ua.com.juja.garazd.sqlcmd;
 
 import ua.com.juja.garazd.sqlcmd.controller.MainController;
 import ua.com.juja.garazd.sqlcmd.model.DatabaseManager;
-import ua.com.juja.garazd.sqlcmd.model.JDBCDatabaseManager;
+import ua.com.juja.garazd.sqlcmd.model.DatabaseManagerImpl;
 import ua.com.juja.garazd.sqlcmd.view.Console;
 import ua.com.juja.garazd.sqlcmd.view.View;
 
 public class Main {
     public static void main(String[] args) {
         View view = new Console();
-        DatabaseManager manager = new JDBCDatabaseManager();
+        DatabaseManager manager = new DatabaseManagerImpl();
 
         MainController controller = new MainController(manager, view);
         controller.run();
