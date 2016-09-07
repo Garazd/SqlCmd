8c40a194346c8507677db3a494b57293982016b9
Garazd <GarazdVZ@gmail.com>
2016-09-07 23:47:39 +0300

    Refactoring class added all new command

--- src/main/java/ua/com/juja/garazd/sqlcmd/controller/command/Help.java
+++ src/main/java/ua/com/juja/garazd/sqlcmd/controller/command/Help.java
@@ -3,7 +3,6 @@ package ua.com.juja.garazd.sqlcmd.controller.command;
 import ua.com.juja.garazd.sqlcmd.view.View;
 
 public class Help implements Command {
-
     private View view;
 
     public Help(View view) {
@@ -19,20 +18,32 @@ public class Help implements Command {
     public void process(String command) {
         view.write("Existing command:");
 
-        view.write("\tlist");
-        view.write("\t\tfor a list of all database tables, is connected to");
+        view.write("\thelp");
+        view.write("\t\tto display the commands list on the screen");
+
+        view.write("\tcreateDatabase|databaseName");
+        view.write("\t\tto create the database with the name");
+
+        view.write("\tdropDatabase|databaseName");
+        view.write("\t\tto drop the database named");
+
+        view.write("\tcreateTable|tableName");
+        view.write("\t\tto create the table with the name");
+
+        view.write("\tdropTable|tableName");
+        view.write("\t\tto drop the table named");
+
+        view.write("\tcreateEntry|tableName|column1|value1|column2|value2|...|columnN|valueN");
+        view.write("\t\tto create entries in the table");
 
         view.write("\tclearTable|tableName");
-        view.write("\t\tto clean up the entire table");
+        view.write("\t\tto clean up the entries table");
 
-        view.write("\tcreateTable|tableName|column1|value1|column2|value2|...|columnN|valueN");
-        view.write("\t\tto createTable a record in the table");
+        view.write("\tshow");
+        view.write("\t\tfor a show of all tables, existing to the database");
 
-        view.write("\tfind|tableName");
+        view.write("\tcontents|tableName");
         view.write("\t\tto get the contents of the table 'tableName'");
-
-        view.write("\thelp");
-        view.write("\t\tto display the list on the screen");
 
         view.write("\texit");
         view.write("\t\tto exit from the program");
