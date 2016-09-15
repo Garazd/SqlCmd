e6aef6f0f976d2b67ebfee7e04fe3867f51f6260
Garazd <GarazdVZ@gmail.com>
2016-09-15 22:30:27 +0300

    Added test class HelpTest

---
+++ src/test/java/ua/com/juja/garazd/sqlcmd/controller/command/HelpTest.java
@@ -0,0 +1,56 @@
+package ua.com.juja.garazd.sqlcmd.controller.command;
+
+import org.junit.Before;
+import org.junit.Test;
+import ua.com.juja.garazd.sqlcmd.model.DatabaseManager;
+import ua.com.juja.garazd.sqlcmd.view.View;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+
+public class HelpTest {
+
+    private DatabaseManager manager;
+    private View view;
+    private Command command;
+
+    @Before
+    public void setup() {
+        manager = mock(DatabaseManager.class);
+        view = mock(View.class);
+        command = new Help(view);
+    }
+
+    @Test
+    public void testCanProcessHelpWithParametersString() {
+        // given
+
+        // when
+        boolean canProcess = command.canProcess("help");
+
+        // then
+        assertTrue(canProcess);
+    }
+
+    @Test
+    public void testCanProcessHelpWithoutParametersString() {
+        // given
+
+        // when
+        boolean canProcess = command.canProcess("");
+
+        // then
+        assertFalse(canProcess);
+    }
+
+    @Test
+    public void testCanProcessQweString() {
+        // given
+
+        // when
+        boolean canProcess = command.canProcess("help|user");
+
+        // then
+        assertFalse(canProcess);
+    }
+}
