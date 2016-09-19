a536a83b125bad911ea7fdd481af2b95fd8239cd
Garazd <GarazdVZ@gmail.com>
2016-09-19 22:59:54 +0300

    Added class IsConnected

---
+++ src/main/java/ua/com/juja/garazd/sqlcmd/controller/command/IsConnected.java
@@ -0,0 +1,26 @@
+package ua.com.juja.garazd.sqlcmd.controller.command;
+
+import ua.com.juja.garazd.sqlcmd.model.DatabaseConnection;
+import ua.com.juja.garazd.sqlcmd.view.View;
+
+public class IsConnected implements Command {
+
+    private DatabaseConnection connection;
+    private View view;
+
+    public IsConnected(DatabaseConnection connection, View view) {
+        this.connection = connection;
+        this.view = view;
+    }
+
+    @Override
+    public boolean canProcess(String command) {
+        return !connection.isConnected();
+    }
+
+    @Override
+    public void process(String command) {
+        view.write(String.format("You can not use the command '%s' is until " +
+            "you connect using commands connect", command));
+    }
+}
