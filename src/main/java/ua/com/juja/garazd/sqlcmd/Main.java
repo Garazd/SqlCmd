6ab84786699894b62362a1e007433adf09e18502
Garazd <GarazdVZ@gmail.com>
2016-09-22 22:20:08 +0300

    Added DatabaseConnection in the main

--- src/main/java/ua/com/juja/garazd/sqlcmd/Main.java
+++ src/main/java/ua/com/juja/garazd/sqlcmd/Main.java
@@ -1,6 +1,8 @@
 package ua.com.juja.garazd.sqlcmd;
 
 import ua.com.juja.garazd.sqlcmd.controller.MainController;
+import ua.com.juja.garazd.sqlcmd.model.DatabaseConnection;
+import ua.com.juja.garazd.sqlcmd.model.DatabaseConnectionImpl;
 import ua.com.juja.garazd.sqlcmd.model.DatabaseManager;
 import ua.com.juja.garazd.sqlcmd.model.DatabaseManagerImpl;
 import ua.com.juja.garazd.sqlcmd.view.Console;
@@ -10,8 +12,9 @@ public class Main {
     public static void main(String[] args) {
         View view = new Console();
         DatabaseManager manager = new DatabaseManagerImpl();
+        DatabaseConnection connection = new DatabaseConnectionImpl();
 
-        MainController controller = new MainController(manager, view);
+        MainController controller = new MainController(connection, manager, view);
         controller.run();
     }
 }
