255720f1f21c1f05fe32a493e205d97513d11d7b
Garazd <GarazdVZ@gmail.com>
2016-10-17 22:15:31 +0300

    Delete class and interface DatabaseConnection

--- src/main/java/ua/com/juja/garazd/sqlcmd/model/DatabaseConnection.java
+++
@@ -1,8 +0,0 @@
-package ua.com.juja.garazd.sqlcmd.model;
-
-public interface DatabaseConnection {
-
-    void connectDatabase(String database, String userName, String password);
-
-    boolean isConnected();
-}
