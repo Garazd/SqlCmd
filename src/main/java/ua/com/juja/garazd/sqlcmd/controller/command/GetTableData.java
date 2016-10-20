e4ecd8116535cdeab5f274549eb39c83c3eee908
Garazd <GarazdVZ@gmail.com>
2016-10-20 23:11:32 +0300

    Refactoring class change view table

--- src/main/java/ua/com/juja/garazd/sqlcmd/controller/command/GetTableData.java
+++ src/main/java/ua/com/juja/garazd/sqlcmd/controller/command/GetTableData.java
@@ -1,9 +1,7 @@
 package ua.com.juja.garazd.sqlcmd.controller.command;
 
-import java.util.List;
-import java.util.Set;
-import ua.com.juja.garazd.sqlcmd.model.DataSet;
 import ua.com.juja.garazd.sqlcmd.model.DatabaseManager;
+import ua.com.juja.garazd.sqlcmd.model.TableConstructor;
 import ua.com.juja.garazd.sqlcmd.view.View;
 
 public class GetTableData implements Command {
@@ -23,38 +21,9 @@ public class GetTableData implements Command {
     @Override
     public void process(String command) {
         String[] data = command.split("\\|");
-        String tableName = data[1];
 
-        Set<String> tableColumns = manager.getTableColumns(tableName);
-        printHeader(tableColumns);
-
-        List<DataSet> tableData = manager.getTableData(tableName);
-        printTable(tableData);
-    }
-
-    private void printTable(List<DataSet> tableData) {
-        for (DataSet row : tableData) {
-            printRow(row);
-        }
-        view.write("--------------------");
-    }
-
-    private void printRow(DataSet row) {
-        List<Object> values = row.getValues();
-        String result = "|";
-        for (Object value : values) {
-            result += value + "|";
-        }
-        view.write(result);
-    }
-
-    private void printHeader(Set<String> tableColmns) {
-        String result = "|";
-        for (String name : tableColmns) {
-            result += name + "|";
-        }
-        view.write("--------------------");
-        view.write(result);
-        view.write("--------------------");
+        TableConstructor constructor = new TableConstructor(
+            manager.getTableColumns(data[1]), manager.getTableData(data[1]));
+        view.write(constructor.getTableString());
     }
 }
