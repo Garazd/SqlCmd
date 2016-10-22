bb80c63a71b41b14b47fac05ba9a8910bc32eae7
Garazd <GarazdVZ@gmail.com>
2016-10-22 21:57:52 +0300

    Refactoring class change import

--- src/test/java/ua/com/juja/garazd/sqlcmd/controller/command/ConnectDatabaseTest.java
+++ src/test/java/ua/com/juja/garazd/sqlcmd/controller/command/ConnectDatabaseTest.java
@@ -4,7 +4,7 @@ import org.junit.Before;
 import org.junit.Test;
 import ua.com.juja.garazd.sqlcmd.model.DatabaseManager;
 import ua.com.juja.garazd.sqlcmd.view.View;
-import static junit.framework.Assert.assertTrue;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
