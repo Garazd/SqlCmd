217fa8125928aa724dc87c787490e1d8b40eaceb
Garazd <GarazdVZ@gmail.com>
2016-10-23 21:06:57 +0300

    Delete JDBCDatabaseManagerTest

--- src/test/java/ua/com/juja/garazd/sqlcmd/model/JDBCDatabaseManagerTest.java
+++
@@ -1,9 +0,0 @@
-package ua.com.juja.garazd.sqlcmd.model;
-
-public class JDBCDatabaseManagerTest extends DatabaseManagerTest {
-
-    @Override
-    public DatabaseManager getDatabaseManager() {
-        return new JDBCDatabaseManager();
-    }
-}
