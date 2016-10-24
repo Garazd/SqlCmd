891f2661df5cf8da9884a959d884880f377c4df3
Garazd <GarazdVZ@gmail.com>
2016-10-24 23:10:26 +0300

    Added new class exception for database

---
+++ src/main/java/ua/com/juja/garazd/sqlcmd/model/DatabaseManagerException.java
@@ -0,0 +1,7 @@
+package ua.com.juja.garazd.sqlcmd.model;
+
+class DatabaseManagerException extends RuntimeException {
+    public DatabaseManagerException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
