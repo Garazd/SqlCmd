582e7cdc00d8fcab2ce185a564120a48ad3cbc4b
Garazd <GarazdVZ@gmail.com>
2016-10-30 10:07:55 +0200

    Refactoring class DatabaseManagerException

--- src/main/java/ua/com/juja/garazd/sqlcmd/model/DatabaseManagerException.java
+++ src/main/java/ua/com/juja/garazd/sqlcmd/model/DatabaseManagerException.java
@@ -1,6 +1,6 @@
 package ua.com.juja.garazd.sqlcmd.model;
 
-class DatabaseManagerException extends RuntimeException {
+public class DatabaseManagerException extends RuntimeException {
     public DatabaseManagerException(String message, Throwable cause) {
         super(message, cause);
     }
