06008af4dae4a71310fedd195eb56f7172c1da71
Garazd <GarazdVZ@gmail.com>
2016-10-30 09:48:33 +0200

    Refactoring class added new test

--- src/test/java/ua/com/juja/garazd/sqlcmd/model/DatabaseManagerTest.java
+++ src/test/java/ua/com/juja/garazd/sqlcmd/model/DatabaseManagerTest.java
@@ -18,8 +18,10 @@ public class DatabaseManagerTest {
     private static String DATABASE_NAME = configurationTest.getDatabaseNameForTest();
     private static String USER_NAME = configurationTest.getUserNameForTest();
     private static String PASSWORD = configurationTest.getPasswordForTest();
+    private static String NOT_EXIST_TABLE = "qwerty";
     private static String TEST_TABLE = "test";
     private static String USERS_TABLE = "users";
+    private static String USERS2_TABLE = "users2";
     private static DatabaseManager manager;
     private static Support support;
 
@@ -33,6 +35,10 @@ public class DatabaseManagerTest {
     @AfterClass
     public static void clearAfterAllTests() {
         support.dropData(manager);
+        manager.dropTable(TEST_TABLE);
+        manager.dropTable(USERS_TABLE);
+        manager.dropTable(USERS2_TABLE);
+        manager.disconnectDatabase();
     }
 
     @Test
@@ -42,7 +48,7 @@ public class DatabaseManagerTest {
         Set<String> tableNames = manager.getTableNames();
 
         // then
-        assertEquals("[users, users2, test]", tableNames.toString());
+        assertEquals("[test, users, users2]", tableNames.toString());
     }
 
     @Test
@@ -62,6 +68,14 @@ public class DatabaseManagerTest {
         Map<String, Object> user = users.get(0);
         assertEquals("[4, Stiven, pass]", user.values().toString());
         assertEquals("[id, name, password]", user.keySet().toString());
+    }
+
+    @Test(expected = DatabaseManagerException.class)
+    public void testExpectedErrorAtGetTableData() {
+        // given
+        // when
+        List<Map<String, Object>> result = manager.getTableData(NOT_EXIST_TABLE);
+        // then
     }
 
     @Test
@@ -86,6 +100,18 @@ public class DatabaseManagerTest {
         Map<String, Object> user = users.get(0);
         assertEquals("[id, name, password]", user.keySet().toString());
         assertEquals("[5, Eva, password]", user.values().toString());
+    }
+
+    @Test(expected = DatabaseManagerException.class)
+    public void testExpectedErrorAtUpdateTableData() {
+        // given
+        Map<String, Object> input = new LinkedHashMap<>();
+        input.put("name", "Stiven");
+        input.put("password", "pass");
+        input.put("id", 5);
+        // when
+        manager.createEntry(NOT_EXIST_TABLE, input);
+        // then
     }
 
     @Test
@@ -129,12 +155,40 @@ public class DatabaseManagerTest {
         assertEquals(0, users.size());
     }
 
+    @Test(expected = DatabaseManagerException.class)
+    public void testExpectedErrorAtClearTable() {
+        // given
+        // when
+        manager.clearTable(NOT_EXIST_TABLE);
+        // then
+    }
+
     @Test
     public void testDropTable() {
         manager.dropTable(TEST_TABLE);
         Set<String> tables = manager.getTableNames();
         assertEquals("[users, users2]", tables.toString());
         manager.createTable("test(id SERIAL NOT NULL PRIMARY KEY,username varchar(225) NOT NULL UNIQUE, password varchar(225))");
+    }
+
+    @Test(expected = DatabaseManagerException.class)
+    public void testExpectedErrorAtDropTable() {
+        // given
+        manager.connectDatabase(DATABASE_NAME, USER_NAME, PASSWORD);
+        manager.createTable(NOT_EXIST_TABLE);
+        // when
+        manager.dropTable(NOT_EXIST_TABLE);
+        // then
+    }
+
+    @Test
+    public void testDatabaseName() {
+        // given
+        // when
+        Set<String> databaseNames = manager.getDatabasesName();
+
+        // then
+        assertEquals("[postgres, sqlcmd, test]", databaseNames.toString());
     }
 
     @Test
