6dd8d89483db62170adacdaf113fc73a96b768d6
Garazd <GarazdVZ@gmail.com>
2016-11-02 23:09:40 +0200

    Refactoring main method because rename class DatabaseManager

--- src/main/java/ua/com/juja/garazd/sqlcmd/Main.java
+++ src/main/java/ua/com/juja/garazd/sqlcmd/Main.java
@@ -2,14 +2,14 @@ package ua.com.juja.garazd.sqlcmd;
 
 import ua.com.juja.garazd.sqlcmd.controller.MainController;
 import ua.com.juja.garazd.sqlcmd.model.DatabaseManager;
-import ua.com.juja.garazd.sqlcmd.model.DatabaseManagerImpl;
+import ua.com.juja.garazd.sqlcmd.model.PostgresDatabaseManager;
 import ua.com.juja.garazd.sqlcmd.view.Console;
 import ua.com.juja.garazd.sqlcmd.view.View;
 
 public class Main {
     public static void main(String[] args) {
         View view = new Console();
-        DatabaseManager manager = new DatabaseManagerImpl();
+        DatabaseManager manager = new PostgresDatabaseManager();
 
         MainController controller = new MainController(manager, view);
         controller.run();
